9869d4aa70e3b7cbdabba622368cac9e6f6f55ae
colloatlas <86520417+colloatlas@users.noreply.github.com>
2021-08-31 16:00:28 +0300

    Add files via upload

---
+++ AttendanceProject.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy
+import face_recognition
+import os
+from datetime import datetime
+
+import numpy as np
+
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+#print(myList)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+#print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+def markaAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime('%H:%m:%S')
+                f.writelines(f'\n{name},{dtString}')
+
+        #print(myDataList)
+
+
+
+encodeListKnown = findEncodings(images)
+#print('Encode Complete')
+
+cap = cv2.VideoCapture(0)
+cap.set(10,100)
+while True:
+    success,img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1*4,y1*4),(x2*4,y2*4), 255,0,0, 2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(255,0,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markaAttendance(name)
+
+    cv2.imshow('Face Recog & Identification',img)
+    if cv2.waitKey(20) & 0xFF ==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
